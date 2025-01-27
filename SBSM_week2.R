d52687fa5d6300698f5df31f5722af7fe67c6fbc
anna.l.2 <ajl21549@gmail.com>
2025-01-27 16:46:38 -0500

    Updated 1/27/25 16:46

---
+++ SBSM_week2.R
@@ -0,0 +1,5 @@
+#Load that dataset.
+#How many sites are there?
+#How many species are there in the entire site-by-species matrix?
+#Any other interesting observations based on what you learned this week?
+  
